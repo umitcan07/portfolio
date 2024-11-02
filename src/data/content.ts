5b1b7530fce0189225064287110cc00c180f4563
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2024-11-02 21:18:52 +0300

    add content for the main section

---
+++ src/data/content.ts
@@ -0,0 +1,77 @@
+export const experiences = [
+  {
+    title: "inploi",
+    label: "Jan 2024 — present",
+    body: "Responsible for developing and maintaining web applications and extensions for the hiring industry in a product-oriented fashion.",
+    link: "https://www.inploi.com/",
+  },
+  {
+    title: "Rex / BabylonAI",
+    label: "Aug 2023 — Oct 2023",
+    body: "Web Application for fitness & exercising. Internship.",
+  },
+  {
+    title: "Winden",
+    label: "Aug 2023 — Oct 2023",
+    body: "Web Application & Website Development for fintech",
+  },
+  {
+    title: "Magna",
+    label: "Aug 2022 — Dec 2022",
+    body: "Web Application & Website Development for web3",
+  },
+  {
+    title: "BlueReceipt / Scale",
+    label: "Aug 2021 — May 2022",
+    body: "Creative Website Development",
+  },
+];
+
+export const extracurriculars = [
+  {
+    title: "Epicons | Design Asset",
+    body: "A set of 2000+ pixel perfect icons",
+    link: "https://www.epicons.co/",
+  },
+  {
+    title: "BuCode | Student Organization",
+    link: "https://bucodecomp.com",
+    label: "Nov 2023 - present",
+    body: "Designer & Developer",
+  },
+  {
+    title: "BUPAL | Student Organization",
+    label: "July 2024 - present",
+    body: "Designer",
+  },
+];
+export const education = [
+  {
+    title: "Boğaziçi University, Istanbul",
+    body: "Computer Engineering",
+    label: "3.53 / 4",
+  },
+  {
+    title: "High School",
+    body: "Science",
+    label: "99.73 / 100",
+  },
+];
+export const personal = [
+  {
+    title: "Mathematics",
+    body: "Algebra, Logic",
+  },
+  {
+    title: "Driving",
+    body: "Sedan cars, 4 lanes, and mild curvature",
+  },
+  {
+    title: "Music",
+    body: "Bright & Electronic, Sia",
+  },
+  {
+    title: "Gaming",
+    body: "VALORANT",
+  },
+];
