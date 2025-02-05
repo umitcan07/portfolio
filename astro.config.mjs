10edcf63c60c9cfc61831dea8b8d1b720767af44
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2025-02-06 02:58:17 +0300

    chore: silly of me not using ssg before

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "static",
   adapter: netlify(),
 });
