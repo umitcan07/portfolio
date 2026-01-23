0bbec69752b7f2bb3aac194e2aec30ea32f217e1
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2026-01-23 10:10:32 +0300

    fix broken link on epicons domain

--- src/data/content.ts
+++ src/data/content.ts
@@ -1,99 +1,99 @@
 export const techStack = [
-  "TypeScript",
-  "React",
-  "Python",
-  "C",
-  "Figma",
-  "Adobe Illustrator",
-  "Webflow",
-  "+999 more...",
+	"TypeScript",
+	"React",
+	"Python",
+	"C",
+	"Figma",
+	"Adobe Illustrator",
+	"Webflow",
+	"+999 more...",
 ];
 
 export const experiences = [
-  {
-    title: "inploi",
-    label: "Jan 2024 — May 2025",
-    body: "Responsible for developing and maintaining web applications and extensions for the hiring industry in a product-oriented fashion.",
-    link: "https://www.inploi.com/",
-  },
-  {
-    title: "Rex / BabylonAI",
-    label: "Aug 2023 — Oct 2023",
-    body: "Web Application for fitness & exercising. Internship.",
-    link: "https://www.rex.fit/",
-  },
-  {
-    title: "Winden",
-    label: "Dec 2022 — Jul 2023",
-    body: "Web Application & Website Development for fintech",
-    link: "https://www.winden.co/",
-  },
-  {
-    title: "Magna",
-    label: "Aug 2022 — Dec 2022",
-    body: "Web Application & Website Development for web3",
-    link: "https://magna.so/",
-  },
-  {
-    title: "BlueReceipt / Scale",
-    label: "Aug 2021 — May 2022",
-    body: "Creative Website Development",
-  },
+	{
+		title: "inploi",
+		label: "Jan 2024 — May 2025",
+		body: "Responsible for developing and maintaining web applications and extensions for the hiring industry in a product-oriented fashion.",
+		link: "https://www.inploi.com/",
+	},
+	{
+		title: "Rex / BabylonAI",
+		label: "Aug 2023 — Oct 2023",
+		body: "Web Application for fitness & exercising. Internship.",
+		link: "https://www.rex.fit/",
+	},
+	{
+		title: "Winden",
+		label: "Dec 2022 — Jul 2023",
+		body: "Web Application & Website Development for fintech",
+		link: "https://www.winden.co/",
+	},
+	{
+		title: "Magna",
+		label: "Aug 2022 — Dec 2022",
+		body: "Web Application & Website Development for web3",
+		link: "https://magna.so/",
+	},
+	{
+		title: "BlueReceipt / Scale",
+		label: "Aug 2021 — May 2022",
+		body: "Creative Website Development",
+	},
 ];
 
 export const extracurriculars = [
-  {
-    title: "Epicons | Design Asset",
-    body: "A set of 2000+ pixel perfect icons",
-    link: "https://www.epicons.co/",
-    label: "2021",
-  },
-  {
-    title: "BuCode | Student Organization",
-    link: "https://bucodecomp.com",
-    label: "2023 - 2024",
-    body: "Designer & Developer",
-  },
-  {
-    title: "BUPAL | Student Organization",
-    label: "July 2024 - Present",
-    body: "Design & Social Media",
-  },
+	{
+		title: "Epicons | Design Asset",
+		body: "A set of 2000+ pixel perfect icons",
+		link: "https://www.epicons.webflow.io/",
+		label: "2021",
+	},
+	{
+		title: "BuCode | Student Organization",
+		link: "https://bucodecomp.com",
+		label: "2023 - 2024",
+		body: "Designer & Developer",
+	},
+	{
+		title: "BUPAL | Student Organization",
+		label: "July 2024 - Present",
+		body: "Design & Social Media",
+	},
 ];
 
 export const education = [
-  {
-    title: "Boğaziçi University, Istanbul",
-    body: "Computer Engineering",
-    label: "3.80 / 4",
-    link: "https://bogazici.edu.tr/",
-  },
-  {
-    title: "High School",
-    body: "Science",
-    label: "99.73 / 100",
-  },
+	{
+		title: "Boğaziçi University, Istanbul",
+		body: "Computer Engineering",
+		label: "3.80 / 4",
+		link: "https://bogazici.edu.tr/",
+	},
+	{
+		title: "High School",
+		body: "Science",
+		label: "99.73 / 100",
+	},
 ];
 
 export const personal = [
-  {
-    title: "Mathematics",
-    body: "Algebra, Logic",
-  },
-  {
-    title: "Driving",
-    body: "Night-driving, sedan, 2-lanes, mild curvature",
-  },
-  {
-    title: "Stargazing",
-    body: "That's how I find the light.",
-  },
-  {
-    title: "Music",
-    body: "Instrumental Post Rock, Drone, Ambient ..:::.. GY!BE",
-  },
-  {
-    title: "Gaming",
-    body: "VALORANT",
-  },
+	{
+		title: "Mathematics",
+		body: "Algebra, Logic",
+	},
+	{
+		title: "Driving",
+		body: "Night-driving, sedan, 2-lanes, mild curvature",
+	},
+	{
+		title: "Stargazing",
+		body: "That's how I find the light.",
+	},
+	{
+		title: "Music",
+		body: "Instrumental Post Rock, Drone, Ambient ..:::.. GY!BE",
+	},
+	{
+		title: "Gaming",
+		body: "VALORANT",
+	},
 ];
